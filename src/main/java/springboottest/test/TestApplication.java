cd2307ffccc3def88ed09ff818fc8a9a981b8b5c
senthilkumarprakasam <sprakas2@volvocars.com>
2018-11-23 15:22:29 +0100

    fixed docker

--- src/main/java/springboottest/test/TestApplication.java
+++ src/main/java/springboottest/test/TestApplication.java
@@ -3,10 +3,23 @@ package springboottest.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
+@RestController
 public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
+	
+	@GetMapping("")
+    public String helloWorld() throws UnknownHostException {
+
+	    return "Hello from " + InetAddress.getLocalHost().getHostName();
+    }
 }
--- src/test/java/springboottest/test/TestApplicationTests.java
+++ src/test/java/springboottest/test/TestApplicationTests.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import springboottest.test.TestApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = TestApplication.class)
 public class TestApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
+	
 }
